2739f43faa6421b94fc84c130ff78146e60bead1
Andrii V <kidstonek@gmail.com>
2022-10-23 21:28:35 +0300

    stats

--- mybill/moneyapp/views.py
+++ mybill/moneyapp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from .models import Category, Expense
 from .forms import CategoryForm, CategoryExpense
 from django.db.models import Sum
+from datetime import datetime
 
 
 # Create your views here.
@@ -22,27 +23,12 @@ def category(request):
     return render(request, 'moneyapp/category.html', {'form': CategoryForm})
 
 
-# def expenses(request):
-#     category = Category.objects.all()
-#     if request.method == 'POST':
-#         try:
-#             form = CategoryExpense(request.POST)
-#             category = Category.objects.all()
-#             new_expense = form.save()
-#             new_expense.category.add('category')
-#         except ValueError as err:
-#             return render(request, 'moneyapp/expenses.html',
-#                           {'form': CategoryExpense, 'error': err, 'categories': category})
-#     category = Category.objects.all()
-#     return render(request, 'moneyapp/expenses.html', {'categories': category})
-
-
 def expenses(request):
     if request.method == 'POST':
         ename = request.POST['ename']
         evalue = request.POST['value']
         category = Category.objects.all()[0]
-        expens = Expense(ename=ename, evalue=evalue,)
+        expens = Expense(ename=ename, evalue=evalue, )
         expens.save()
         expens.category.add(category)
     category = Category.objects.all()
@@ -50,6 +36,15 @@ def expenses(request):
 
 
 def stats(request):
-    expense = Expense.objects.all()
-    sum_ = Expense.objects.aggregate(Sum('evalue'))
-    return render(request, 'moneyapp/stats.html', {'expenses': expense, 'sum_': sum_['evalue__sum']})
+    if request.method == 'POST':
+        first_date = request.POST.get('date_from')
+        end_date = request.POST.get('date_for')
+        try:
+            fd = datetime.strptime(first_date, "%Y-%m-%d")
+            ed = datetime.strptime(end_date, "%Y-%m-%d")
+        except TypeError:
+            ed = datetime.now().date()
+        rep = Expense.objects.filter(edate__range=[fd, ed]).aggregate(Sum('evalue'))
+        return render(request, 'moneyapp/stats.html', {'first_date': first_date, 'end_date': end_date, 'rep': rep['evalue__sum']})
+    else:
+        return render(request, 'moneyapp/stats.html', {})
