8b0a9abae224b78c85eefcb4704f2f19607e7a72
kidstonek <96789294+kidstonek@users.noreply.github.com>
2022-11-01 08:57:50 +0200

    Update settings.py
    secret key - deleted

--- mybill/mybill/settings.py
+++ mybill/mybill/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-gl76l$k7iielz00@=-^9rngv!&ut555ai#vhrw^2e&aokp9xvp'
+SECRET_KEY = 
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
